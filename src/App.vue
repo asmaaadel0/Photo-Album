82b7bef75a4ea2f06ad18926393ad6568e12ebb9
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-04-19 22:59:01 +0200

    update initialization

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheForm from './components/TheForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheForm
   }
 }
 </script>
